13bf1abc6581ac5cc7373bb92d7f6b4a42067f87
Rivibibu Prabashwara <92269011+WKprabashwara@users.noreply.github.com>
2022-05-15 22:12:10 +0530

    Add files via upload

---
+++ PhixyBot/modules/helper_funcs/string_store.py
@@ -0,0 +1,51 @@
+PHIXY = [
+    "Yeah Babe Tell me.",
+    "Something went wrong.",
+    "Something went wrong.",
+    "Something went wrong.",
+    "Something went wrong.",
+    "Something went wrong.",
+    "Nope",
+    "Maybe",
+    "Nope",
+    "Something went wrong.",
+    "Something went wrong.",
+    "Nope",
+    "Nope",
+    "Nope",
+    "Nope",
+    "Something went wrong.",
+    "Maybe",
+    "Something went wrong.",
+    "Nope",
+    "Something went wrong",
+    "Maybe",
+    "Something went wrong",
+    "Something went wrong",
+    "Something went wrong",
+    "Something went wrong",
+    "Nope",
+    "Nope",
+    "Maybe",
+    "Nope",
+    "Something went wrong",
+    "Something went wrong",
+    "Something went wrong",
+    "Something went wrong",
+    "Nope",
+    "Something went wrong",
+    "Maybe",
+    "Nope",
+]
+
+GDMORNING = [
+    "Good Morning Babe 🥱⛅️🌈",
+]
+
+GDNIGHT = [
+    "Good Night Babe ☺️🌒✨",
+]
+
+PRABASHWARA = [
+    "Oh! 🤯",
+]
